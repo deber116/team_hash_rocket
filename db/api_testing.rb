c779d1dc882847eec9fd59669f4ade4c0d756649
deber116 <deber116@gmail.com>
2020-03-02 15:58:49 -0600

    added first 50 pokemon from parsed data in seeds

--- db/api_testing.rb
+++ db/api_testing.rb
@@ -1,27 +1,8 @@
-require 'open-uri'
 require 'net/http'
+require 'open-uri'
 require 'json'
 require_relative '../config/environment.rb'
 
-#API_BASE = "https://pokeapi.co/"
-
-
-#url = API_BASE + 
-#uri = URI.parse(url)
-
-Trainer.destroy_all
-Pokemon.destroy_all
-Type.destroy_all
-TrainedPokemon.destroy_all
-
-# elec = Type.create(name: "electric")
-# pikachu = Pokemon.create(name: "Pikachu",type: elec)
-# ash = Trainer.create(name: "Ash")
-
-# trained = TrainedPokemon.create(nickname: "testy", pokemon: pikachu, trainer: ash)
-
-
-# --- NEW CODE ---
 
 API_BASE = 'https://pokeapi.co/api/v2/'
 
