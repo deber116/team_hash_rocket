8362f34440db22d48508c5455133758e9dca0dd4
ellisandrews <ellisandrews1@gmail.com>
2020-03-03 13:37:22 -0600

    Use tty-prompt instead of gets.chomp

--- app/models/trained_pokemon.rb
+++ app/models/trained_pokemon.rb
@@ -2,4 +2,8 @@ class TrainedPokemon < ActiveRecord::Base
     belongs_to :trainer
     belongs_to :pokemon
     has_one :type, through: :pokemon
+
+    def to_s
+        "#{self.nickname} (#{self.pokemon.name})"
+    end
 end
--- config/environment.rb
+++ config/environment.rb
@@ -1,11 +1,13 @@
 require 'bundler'
 Bundler.require
 
-
+require 'tty-prompt'
+$prompt = TTY::Prompt.new
 
 ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: 'db/development.db')
 require_all 'lib'
 require_all 'app/models'
 
 # Added to disable DEBUG level logging (e.g. SQL statements)
-# ActiveRecord::Base.logger = nil
+ ActiveRecord::Base.logger = nil
+
