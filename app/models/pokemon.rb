cd9f96c6a91509c5f46968af47a81e2c758f5b6d
deber116 <deber116@gmail.com>
2020-03-04 09:17:13 -0600

    added seeding of moves and pokemon moves

--- app/models/pokemon.rb
+++ app/models/pokemon.rb
@@ -2,7 +2,8 @@ class Pokemon < ActiveRecord::Base
     has_many :trained_pokemons
     has_many :trainers, through: :trained_pokemons
     belongs_to :type
-
+    has_many :pokemon_moves
+    has_many :moves, through: :pokemon_moves
     def self.list_all
         Pokemon.all.each_with_index do |pokemon, i|
             puts "#{i+1}. #{pokemon}"
--- db/seeds.rb
+++ db/seeds.rb
@@ -19,6 +19,7 @@ def get_move_data(pokemon_data)
     move_list = move_list.map do |move|
         move["move"]["name"]
     end
+    
 end
 
 def get_evolutions(evolution_chain_id)
@@ -34,8 +35,10 @@ def get_evolutions(evolution_chain_id)
     base_pokemon_data = get_pokemon_info(base_pokemon_name)
     
     #getting move data for base_pokemon_name
-    get_move_data(base_pokemon_data)
+    move_data = get_move_data(base_pokemon_data)
     #PENDING: Above method does not yet seed data to move tables
+
+    
 
     base_pokemon_first_type = base_pokemon_data['types'].find { |type| type['slot'] == 1 }
     base_pokemon_type = base_pokemon_first_type['type']['name']
@@ -44,6 +47,12 @@ def get_evolutions(evolution_chain_id)
     chain_type = Type.find_or_create_by(name: base_pokemon_type)
     base_pokemon = Pokemon.create(name: base_pokemon_name, type: chain_type)
     chain = data['chain']['evolves_to']
+
+    move_data.each do |move|
+        new_move = Move.find_or_create_by(name: move)
+        base_pokemon.moves << new_move
+    end
+
     if chain[0]
         base_pokemon.next_evolution_id = base_pokemon.id + 1
         base_pokemon.save
