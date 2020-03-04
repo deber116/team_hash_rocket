6a955f33a36480bb35e9c656596fe5988a93bb84
deber116 <deber116@gmail.com>
2020-03-04 14:11:51 -0600

    updated run file, and added helper methods to trainer and trained_pokemon models

--- app/models/trained_pokemon.rb
+++ app/models/trained_pokemon.rb
@@ -2,8 +2,26 @@ class TrainedPokemon < ActiveRecord::Base
     belongs_to :trainer
     belongs_to :pokemon
     has_one :type, through: :pokemon
-
+    has_many :trained_pokemon_moves
+    has_many :moves, through: :trained_pokemon_moves
+    
     def to_s
         "#{self.nickname} (#{self.pokemon.name})"
+    end
+
+    def get_moves_names
+        move_names = self.pokemon.moves.map {|move| move.name}
+    end
+
+    def add_move(move)
+        self.moves << move
+    end
+
+    def trained_moves
+        self.trained_pokemon_moves
+    end
+
+    def trained_moves_names
+        self.trained_moves.map {|trained_move| trained_move.move.name}
     end
 end
