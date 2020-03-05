90cccee63298b93d131ff0eb77a3b18d0ca65073
deber116 <deber116@gmail.com>
2020-03-05 11:28:13 -0600

    colorized prompts

--- app/models/trainer.rb
+++ app/models/trainer.rb
@@ -3,35 +3,35 @@ class Trainer < ActiveRecord::Base
     has_many :pokemons, through: :trained_pokemons
 
     def add_trained_pokemon(pokemon)
-        nickname = $prompt.ask("What would you like to nickname it?")
+        nickname = $prompt.ask("What would you like to nickname it?".yellow)
         TrainedPokemon.create(nickname: nickname, trainer: self, pokemon: pokemon)
-        puts "Congrats #{self.name}, #{nickname} has been added to your team!"
+        puts "Congrats #{self.name}, #{nickname} has been added to your team!".light_green
     end
 
     def catch(pokemon)
 
         if self.trained_pokemons.count >= 6
-            response = $prompt.yes?("You have too many pokemon. Would you like to release one of the pokemon on your team?")
+            response = $prompt.yes?("You have too many pokemon. Would you like to release one of the pokemon on your team?".yellow)
             
             if response == true
                 self.release
-                puts "You caught a #{pokemon.name}!"
+                puts "You caught a #{pokemon.name}!".light_green
                 self.add_trained_pokemon(pokemon)
             else 
-                puts "The pokemon got away!"
+                puts "The pokemon got away!".red
             end
         
         else
-            puts "You caught a #{pokemon.name}!"
+            puts "You caught a #{pokemon.name}!".light_green
             self.add_trained_pokemon(pokemon)
         end
     end
 
     def release
         team = self.list_team
-        tp_to_release = $prompt.select("Which pokemon on your team would you like to release?", team)
+        tp_to_release = $prompt.select("Which pokemon on your team would you like to release?".yellow, team)
         tp_to_release.destroy
-        puts "#{tp_to_release.nickname} was released."
+        puts "#{tp_to_release.nickname} was released.".yellow
     end
 
     def list_team
--- bin/run.rb
+++ bin/run.rb
@@ -1,18 +1,18 @@
 require_relative '../config/environment'
 
 puts "Hello! Sorry to keep you waiting! Welcome to the world of POKEMON!
-My name is OAK. People call me the POKEMON PROF."
+My name is OAK. People call me the POKEMON PROF.".light_magenta
 
 puts "\nThis world is inhabited by creatures that we call POKEMON.
 People and POKEMON live together by supporting each other.
-Some people play with POKEMON, some battle with them."
+Some people play with POKEMON, some battle with them.".light_magenta
 
 puts "\nBut we don't know everything about POKEMON yet. There are still
-many mysteries to solve. That's why I study POKEMON every day."
+many mysteries to solve. That's why I study POKEMON every day.".light_magenta
 puts ''
 
 # Get the Trainer's name. Convert their input to Title Case (e.g. "eLlis aNdReWs" --> "Ellis Andrews")
-name = $prompt.ask("Now, what did you say your name was? =>") { |q| q.modify :collapse, :trim, :down }.titleize 
+name = $prompt.ask("Now, what did you say your name was? =>".cyan) { |q| q.modify :collapse, :trim, :down }.titleize 
 
 # Find an existing trainer or create a new one if none exists in the database
 trainer = Trainer.find_or_create_by(name: name)
@@ -20,28 +20,28 @@ trainer = Trainer.find_or_create_by(name: name)
 while true do
     # Ask the trainer what action they would like to take
     choices = ["Look for wild pokemon", "View Pokedex", "View your team of pokemon","Teach moves to pokemon on your team","Check what moves your pokemon have","Quit"]
-    selection = $prompt.select("#{trainer.name}, what would you like to do now?", choices)
+    selection = $prompt.select("#{trainer.name}, what would you like to do now?".yellow, choices)
 
     if selection == "Look for wild pokemon"
         # Return a random pokemon from the db
         pokemon = Pokemon.all.sample
-        response = $prompt.yes?("A wild #{pokemon.name} appeared! Do you want to catch it?")
+        response = $prompt.yes?("A wild #{pokemon.name} appeared! Do you want to catch it?".light_green)
 
         if response == true
             trainer.catch(pokemon)
         else
-            puts "The pokemon got away!"
+            puts "The pokemon got away!".red
         end
 
     elsif selection == "View Pokedex"
 
         pokedex_commands = ["List all pokemon", "View pokemon by type"]
-        command = $prompt.select("What would you like to check on your Pokedex?", pokedex_commands)
+        command = $prompt.select("What would you like to check on your Pokedex?".yellow, pokedex_commands)
         if command == "List all pokemon"
             Pokemon.list_all
         elsif command == "View pokemon by type"
             types = Type.list_types_by_name
-            type_str = $prompt.select("Which type would you like to view?", types)
+            type_str = $prompt.select("Which type would you like to view?".yellow, types)
             type = Type.find_by(name: type_str)
             Pokemon.list_all_by_type(type)
         end
@@ -50,19 +50,19 @@ while true do
         trainer.list_team
     elsif selection == "Teach moves to pokemon on your team"
         if trainer.get_team.count == 0
-            puts "You don't have any members on your team yet. Go catch some pokemon!"
+            puts "You don't have any members on your team yet. Go catch some pokemon!".yellow
 
         else
-            p_selection = $prompt.select("Which pokemon on your team would you like to teach a move?", trainer.get_team_choices_hash)
+            p_selection = $prompt.select("Which pokemon on your team would you like to teach a move?".yellow, trainer.get_team_choices_hash)
             t_pokemon = trainer.trained_pokemons[p_selection]
 
             if t_pokemon.trained_moves.count == 4
-                response = $prompt.yes?("That pokemon already knows 4 moves. Would you like to delete one?")
+                response = $prompt.yes?("That pokemon already knows 4 moves. Would you like to delete one?".yellow)
                 if response
                     move_instances = t_pokemon.trained_moves
                     move_names = t_pokemon.trained_moves_names
                     move_hash = Hash[move_names.zip move_instances]
-                    result = $prompt.select("Which move would you like to delete?", move_hash)
+                    result = $prompt.select("Which move would you like to delete?".yellow, move_hash)
                     #binding.pry
                     result.destroy
                 end
@@ -76,7 +76,7 @@ while true do
                 moves_hash = Hash[remaining_names.zip remaining_instances]
                 
                 #returns move instance that needs to be added
-                selected_move_instance = $prompt.select("Which move would you like to teach this pokemon?", moves_hash)
+                selected_move_instance = $prompt.select("Which move would you like to teach this pokemon?".yellow, moves_hash)
                 
                 t_pokemon.add_move(selected_move_instance) 
                 #binding.pry
@@ -84,10 +84,16 @@ while true do
         end
     elsif selection == "Check what moves your pokemon have"
         if trainer.get_team.count == 0
-            puts "You don't have any members on your team yet. Go catch some pokemon!"
+            puts "You don't have any members on your team yet. Go catch some pokemon!".yellow
         else
-            check_pokemon_index = $prompt.select("Which pokemon would you like to check the moves of?",trainer.get_team_choices_hash)
-            puts trainer.trained_pokemons[check_pokemon_index].trained_moves_names
+            check_pokemon_index = $prompt.select("Which pokemon would you like to check the moves of?".yellow,trainer.get_team_choices_hash)
+            t_pokemon = trainer.trained_pokemons[check_pokemon_index]
+            if trainer.trained_pokemons[check_pokemon_index].trained_moves_names.count == 0
+                puts "This pokemon doesn't have any moves yet. Teach it some moves!".yellow
+            else
+                puts "Here are the moves that #{t_pokemon.nickname} knows.".yellow
+                puts trainer.trained_pokemons[check_pokemon_index].trained_moves_names
+            end
         end
     else
         break
